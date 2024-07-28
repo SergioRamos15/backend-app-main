3ea6851432834f13baadc2f465e322a6c2dc0a5d
SergioRRB <sergioraulramos276@gmail.com>
2024-07-27 19:34:04 -0500

    Backend 50%

--- src/controllers/selectRecojoController.ts
+++ src/controllers/selectRecojoController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { recojoSeleccionado } from '../models/consults/selectRecojo.model';
-import { IGuiaRecojo } from '../interfaces/Guias';
+import { IGuiaRecojo, IRegistroCarga } from '../interfaces/Guias';
 
 export const guiaSeleccionadoRecojo = async (req: Request, res: Response): Promise<Response> => {
   const { id, id_orden, tipo_usuario } = req.params;
@@ -13,13 +13,17 @@ export const guiaSeleccionadoRecojo = async (req: Request, res: Response): Promi
     if (!dataGuia || dataGuia.length === 0) {
       return res.status(404).json({ message: "Guia no encontrada" })
     }
-
     console.log(dataGuia)
-
     dataGuiaRecojo = dataGuia.find(guia => guia.id_orden_servicio_recojo === id_orden);
-
     console.log(dataGuiaRecojo);
-    return res.status(200).json({ dataGuiaRecojo });
+
+    const dataRegistroCarga: IRegistroCarga[] = await recojoSeleccionado.extraerWithIdOrden(id_orden);
+
+    const id_destino = dataRegistroCarga[0].id_destino_registro_carga;
+
+    const dataGuiaRecojoSelected = await recojoSeleccionado.dataUserGuias(id_orden, id_destino);
+
+    return res.status(200).json({ dataGuiaRecojo, dataGuiaRecojoSelected });
 
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
--- src/models/consults/selectRecojo.model.ts
+++ src/models/consults/selectRecojo.model.ts
@@ -1,15 +1,16 @@
 import { AppDataSource } from "../../config/dbConfig";
 import { queriesSelectRecojo } from "../../sql/recojo.queries";
-import { IGuiaRecojo } from "../../interfaces/Guias";
+import { queriesSelectEnvio } from "../../sql/envio.queries";
+import { IGuiaRecojo, IGuiaEnvioSelected, IRegistroCarga } from "../../interfaces/Guias";
 
 export const recojoSeleccionado = {
   async dataRecojo(id: number, id_orden: string, tipo_usuario: string): Promise<IGuiaRecojo[]> {
 
     let query: string;
 
-    if(tipo_usuario === 'proveedor') {
+    if (tipo_usuario === 'proveedor') {
       query = queriesSelectRecojo.proveedor;
-    } else if(tipo_usuario === 'auxiliar') {
+    } else if (tipo_usuario === 'auxiliar') {
       query = queriesSelectRecojo.auxiliar;
     } else {
       throw new Error("Tipo de usuario no reconocido");
@@ -19,5 +20,35 @@ export const recojoSeleccionado = {
     const dataRecojoGuia = await AppDataSource.query(query, [id, id_orden]);
     return dataRecojoGuia;
 
-  }
+  },
+
+  async extraerWithIdOrden(id_orden: string): Promise<IRegistroCarga[]> {
+    const dataRecojo = await AppDataSource.query("SELECT * FROM registros_cargas WHERE id_orden_servicio_registro_carga = ?", [id_orden])
+    return dataRecojo;
+  },
+
+  async dataUserGuias(id_orden: string, id_destino: string): Promise<IGuiaEnvioSelected[]> {
+    const cod = id_orden.split('-');
+    let queryIndex: number;
+
+    if (cod[0] === 'C1') {
+      queryIndex = 0;
+    } else if (cod[0] === 'C2') {
+      console.log("SALIO C2")
+      queryIndex = 1;
+    } else if (cod[0] === 'OS1') {
+      queryIndex = 2;
+    } else if (cod[0] === 'OS2') {
+      queryIndex = 3;
+    } else {
+      throw new Error("Prefijo de id_orden no reconocido")
+    }
+
+    const query = queriesSelectEnvio[queryIndex];
+    console.log("QUERY: ", queryIndex);
+    console.log(query);
+    const dataEnvioGuia = await AppDataSource.query(query, [id_orden, id_destino]);
+    return dataEnvioGuia;
+  },
+
 }
